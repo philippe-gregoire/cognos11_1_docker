edbdd258ac111dc3dae0dc5dd7b3c04512cd4923
Philippe Gregoire <philippe.gregoire@fr.ibm.com>
2019-03-08 20:08:54 +0100

    Added retries on Cognos Startup

--- phg-cnts6-cognos-cloudera/phg-centos7-xfce-cognos/cognosca/cogsetup_wait.sh
+++ phg-cnts6-cognos-cloudera/phg-centos7-xfce-cognos/cognosca/cogsetup_wait.sh
@@ -6,25 +6,37 @@ if [[ -z "${COGNOS_WAIT_DB2_MINS}" ]]; then
   echo "Using default DB2 wait time ${COGNOS_WAIT_DB2_MINS}"
 fi
 
-echo "--------------------------------------------------------"
-echo "Waiting ${COGNOS_WAIT_DB2_MINS} minutes to get DB2 time to startup"
-sleep ${COGNOS_WAIT_DB2_MINS}m
-
-echo "--------------------------------------------------------"
 echo "Renaming cogstartup.xml* files in ${COGNOS_DIR}"
 mv ${COGNOS_DIR}/configuration/cogstartup.xml ${COGNOS_DIR}/configuration/cogstartup.xml_backup
 cp ${COGNOS_DIR}/configuration/cogstartup.xml_configured ${COGNOS_DIR}/configuration/cogstartup.xml
 
 echo "--------------------------------------------------------"
-echo "Running unattended config"
+retriesLeft=${COGNOS_MAX_RETRIES}
+CONFIG_STATUS=3
+while [ $retriesLeft -gt 0 -a $CONFIG_STATUS -ne 0 ]; do
+echo "======= Attempting to start Cognos ${retriesLeft} times ========="
+
+waitcnt=${COGNOS_WAIT_DB2_MINS}
+while [ $waitcnt -gt 0 ]; do
+	echo "Waiting ${waitcnt} minutes to get DB2 time to startup"
+	sleep 1m
+	waitcnt=$(( $waitcnt - 1 ))
+done
+
+echo "====== Running Cognos Unattended config =================="
 cd ${COGNOS_DIR}/bin64
 ./cogconfig.sh -s
-
-echo "--------------------------------------------------------"
 CONFIG_STATUS=$?
-echo "Config status: ${CONFIG_STATUS}"
+echo "====== Cognos configuration status: ${CONFIG_STATUS} ====="
+if [ $CONFIG_STATUS -ne 0 ]; then
+	cat ${COGNOS_DIR}/logs/cogconfig_response.csv
+fi
 
-tail -f ${COGNOS_DIR}/logs/cogconfig_response.csv
+retriesLeft=$(( $retriesLeft - 1 ))
+done
 
-ps -ef
-
+if [ $CONFIG_STATUS -eq 0 ]; then
+	tail -f ${COGNOS_DIR}/logs/cogconfig_response.csv
+else
+	echo "Too many retries, exiting "
+fi
