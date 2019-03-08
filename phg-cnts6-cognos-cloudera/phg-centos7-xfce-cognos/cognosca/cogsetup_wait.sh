c7ca7ab3961db128af0ffdab66f9737b1cd23f17
Philippe Gregoire <philippe.gregoire@fr.ibm.com>
2019-03-08 18:38:16 +0100

    Add variable to wait for DB2 from Cognos

--- phg-cnts6-cognos-cloudera/phg-centos7-xfce-cognos/cognosca/cogsetup_wait.sh
+++ phg-cnts6-cognos-cloudera/phg-centos7-xfce-cognos/cognosca/cogsetup_wait.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 set +e
 
-uptime
-echo "Waiting 4 minutes to get DB2 time to startup"
-sleep 4m
+if [[ -z "${COGNOS_WAIT_DB2_MINS}" ]]; then
+  COGNOS_WAIT_DB2_MINS=${COGNOS_WAIT_DB2_MINS_DEF}
+  echo "Using default DB2 wait time ${COGNOS_WAIT_DB2_MINS}"
+fi
 
+echo "--------------------------------------------------------"
+echo "Waiting ${COGNOS_WAIT_DB2_MINS} minutes to get DB2 time to startup"
+sleep ${COGNOS_WAIT_DB2_MINS}m
+
+echo "--------------------------------------------------------"
 echo "Renaming cogstartup.xml* files in ${COGNOS_DIR}"
 mv ${COGNOS_DIR}/configuration/cogstartup.xml ${COGNOS_DIR}/configuration/cogstartup.xml_backup
 cp ${COGNOS_DIR}/configuration/cogstartup.xml_configured ${COGNOS_DIR}/configuration/cogstartup.xml
 
+echo "--------------------------------------------------------"
 echo "Running unattended config"
 cd ${COGNOS_DIR}/bin64
 ./cogconfig.sh -s
+
+echo "--------------------------------------------------------"
+CONFIG_STATUS=$?
+echo "Config status: ${CONFIG_STATUS}"
 
 tail -f ${COGNOS_DIR}/logs/cogconfig_response.csv
 
